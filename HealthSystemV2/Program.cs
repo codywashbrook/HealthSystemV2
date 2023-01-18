7a5c8d9fa03747bfbad2dac7763fc1d6207bfa2c
codywashbrook <codywashbrook@hotmail.com>
2023-01-18 08:40:21 -0400

    Initial commit

---
+++ HealthSystemV2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Health System V2");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
